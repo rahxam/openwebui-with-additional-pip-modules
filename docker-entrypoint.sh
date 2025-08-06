f97207a2e6588be9a5d21374bf9f01b93402bde8
Hartig <maximilian.hartig@aequitas-group.de>
2025-08-06 15:35:53 +0200

    call start.sh script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ -n "$EXTRA_PYTHON_MODULES" ]; then
     echo "Installing extra python modules: $EXTRA_PYTHON_MODULES"
-    pip install --no-cache-dir $EXTRA_PYTHON_MODULES
+    pip3 install --no-cache-dir $EXTRA_PYTHON_MODULES
 fi
 
 # Exec the original CMD or pass all arguments
-exec "$@"
+exec "bash start.sh"
